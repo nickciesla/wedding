5ebb19a641f07b4fadeef2b44c439edbb8ad3f7a
nickciesla <nicholasciesla@gmail.com>
2023-10-17 22:35:26 -0400

    fix onload

--- js/starter.js
+++ js/starter.js
@@ -1,6 +1,6 @@
 window.onload = function () {
-	var line = document.getElementById('fh5co-header');
-	const page_height = window.outerHeight * .5
+	var line = document.getElementById('fh5co-header-home');
+	const page_height = window.outerHeight
 	if (line) {
 		line.style.height = page_height + 'px';
 	}
